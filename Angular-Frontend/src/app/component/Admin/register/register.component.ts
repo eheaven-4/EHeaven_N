dd4b0bb28299f0762894e49e152e8362cbf89063
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-09-02 22:35:58 +0530

    Install multer

--- Angular-Frontend/src/app/component/Admin/register/register.component.ts
+++ Angular-Frontend/src/app/component/Admin/register/register.component.ts
@@ -37,42 +37,47 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
-    registerUser(){
-      const user = {
-        usertype: this.usertype,
-        userid: this.userid,
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        birthday: this.birthday,
-        mobilenumber: this.mobilenumber,
-        homenumber: this.homenumber,
-        gender: this.gender,
-        nationality: this.nationality,
-        nicnumber: this.nicnumber,
-        father: this.father,
-        mother: this.mother,
-        address: this.address,
-      }
+  selectedFile: File = null;
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+  }
 
-        var url = "http://localhost:3000/users/register";
-        this.http.post<any>(url,user).subscribe(res => {
-          if(res.state){
-            console.log(res.msg);
-            alert("Successfully registerd");
-            this.router.navigate(['/login']);
-          }
-          else{
-            // this.ngFlashMessageService.showFlashMessage({
-            //   messages: ["You are not Registerd"],
-            //   dismissible: true, 
-            //   timeout: 2000,
-            //   type: 'warning'
-            // });
-            console.log(res.msg);
-            alert("Successfully registerd");
-            this.router.navigate(['/register']);
-          }
-      });
+  registerUser(){
+    const user = {
+      usertype: this.usertype,
+      userid: this.userid,
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      birthday: this.birthday,
+      mobilenumber: this.mobilenumber,
+      homenumber: this.homenumber,
+      gender: this.gender,
+      nationality: this.nationality,
+      nicnumber: this.nicnumber,
+      father: this.father,
+      mother: this.mother,
+      address: this.address,
+    }
+
+      var url = "http://localhost:3000/users/register";
+      this.http.post<any>(url,user).subscribe(res => {
+        if(res.state){
+          console.log(res.msg);
+          alert("Successfully registerd");
+          this.router.navigate(['/login']);
+        }
+        else{
+          // this.ngFlashMessageService.showFlashMessage({
+          //   messages: ["You are not Registerd"],
+          //   dismissible: true, 
+          //   timeout: 2000,
+          //   type: 'warning'
+          // });
+          console.log(res.msg);
+          alert("Successfully registerd");
+          this.router.navigate(['/register']);
+        }
+    });
   }
 }
