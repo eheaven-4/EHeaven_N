9aaee3293b75e53cb3eb948927adfb69674d9d2c
lalinda <nawinjith96@gmail.com>
2019-09-20 23:47:41 +0530

    certification- initial form controls set

--- Angular-Frontend/src/app/component/Base_Components/certification/certification.component.ts
+++ Angular-Frontend/src/app/component/Base_Components/certification/certification.component.ts
@@ -9,10 +9,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CertificationComponent implements OnInit {
 
+  constructor() { }
+
   CertificationForm = new FormGroup({
     certName: new FormControl(''),
-
+    certType: new FormControl(''),
+    exam:     new FormGroup({
+      examName: new FormControl(''),
+      examYear: new FormControl(''),
+      examIndex: new FormControl('')
+    })
   });
+          // add function to get logged username by default 
 
   // certificate types
   certificates = ['Studentship Confirmation Certificate','Character Certificate','Leaving Certificate','Educational Certificate'];
@@ -21,7 +29,7 @@ export class CertificationComponent implements OnInit {
   exams = ['Grade 05 Scholarship Examination','Ordinary Level ( G.C.E. O/L ) Examination','Advanced Level ( G.C.E. A/L ) Examination'];
   yearofExam = ['2010','2011','2012','2013','2014','2015','2016','2017','2018'];
 
-  constructor() { }
+
 
 
   ngOnInit() {
