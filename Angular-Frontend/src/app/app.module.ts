bcd7cae809eea4b3af51aa1afe1e0be5cb859c64
lalinda <nawinjith96@gmail.com>
2019-09-24 11:20:58 +0530

    Update: prepare-certificates

--- Angular-Frontend/src/app/app.module.ts
+++ Angular-Frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgFlashMessagesModule } from 'ng-flash-messages';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatModule } from './material.theme';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -56,6 +58,7 @@ import { PrepareCertificationComponent } from './component/Admin/prepare-certifi
     ReactiveFormsModule,
     NgFlashMessagesModule,
     MatModule,
+    MatExpansionModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
   ],
--- Angular-Frontend/src/app/component/Admin/prepare-certification/prepare-certification.component.ts
+++ Angular-Frontend/src/app/component/Admin/prepare-certification/prepare-certification.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-prepare-certification',
