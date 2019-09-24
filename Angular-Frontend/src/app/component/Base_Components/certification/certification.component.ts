81ae999f861690b9b98e837191d5a8c7beb7fe6b
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-09-25 00:27:24 +0530

    Updates in Certification

--- Angular-Frontend/src/app/component/Base_Components/certification/certification.component.ts
+++ Angular-Frontend/src/app/component/Base_Components/certification/certification.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
-
+import { MycookiesService } from '../../Admin/mycookies.service';
+import { StickyDirection } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-certification',
@@ -14,68 +14,74 @@ export class CertificationComponent implements OnInit {
   value: String = '';
   flag = false;
 
+  userid: String;
+  userName: String;
+  certName: String;
+  examination: String;
+  examIndex: String;
+  examYear: String;
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private http: HttpClient
+    private http: HttpClient,
+    private cookies: MycookiesService,
 
-
-    ) { }
+  ) { }
 
   CertificationForm = this.fb.group({
     certName: ['', Validators.required],
     certType: ['', Validators.required],
     exam: this.fb.group({
-       examName: ['', Validators.required],
-       examYear: ['', Validators.required],
-       examIndex: ['', Validators.required]
+      examName: ['', Validators.required],
+      examYear: ['', Validators.required],
+      examIndex: ['', Validators.required]
     })
   });
 
-  // CertificationForm = new FormGroup({
-  //   certName: new FormControl(''),
-  //   certType: new FormControl(''),
-  //   exam:     new FormGroup({
-  //     examName: new FormControl(''),
-  //     examYear: new FormControl(''),
-  //     examIndex: new FormControl('')
-  //   })
-  // });
-          // add function to get logged username by default
-
   // certificate types
-  certificates = ['Student Status Verification Certificate','Character Certificate','Leaving Certificate','Educational Certificate'];
+  certificates = ['Student Status Verification Certificate', 'Character Certificate', 'Leaving Certificate', 'Educational Certificate'];
 
   // examinations
-  exams = ['Grade 05 Scholarship Examination','Ordinary Level ( G.C.E. O/L ) Examination','Advanced Level ( G.C.E. A/L ) Examination'];
-  yearofExam = ['2010','2011','2012','2013','2014','2015','2016','2017','2018'];
-
-
-
+  exams = ['Grade 05 Scholarship Examination', 'Ordinary Level ( G.C.E. O/L ) Examination', 'Advanced Level ( G.C.E. A/L ) Examination'];
+  yearofExam = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
 
   ngOnInit() {
   }
 
+  submitToApproval() {
+    var myCookie = JSON.parse(this.cookies.getCookie("userAuth"));
+    console.log(myCookie.usertype)
+    const certRequest = {
+      userid: this.userid,
+      userName: this.userName,
+      certName: this.certName,
+      examination: this.examination,
+      examIndex: this.examIndex,
+      examYear: this.examYear
+    }
+    console.log(certRequest);
+  }
   // on submit
 
-  applyCertificates(){
+  applyCertificates() {
     console.log(this.CertificationForm.value);
     alert('Applied Sucessfully');
     this.CertificationForm.reset();
-    //  this.router.navigate(['certification']);
   }
 
   // used to show/hide form fields
 
   testfunction(value) {
-    if(value == "Educational Certificate"){
-       this.flag = true;
+    if (value == "Educational Certificate") {
+      this.flag = true;
 
     }
-    else{
-       this.flag = false;
+    else {
+      this.flag = false;
 
     }
 
   }
+
 }
