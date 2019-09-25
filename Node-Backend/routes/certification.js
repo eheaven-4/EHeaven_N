d9c817542817946bd350f63771577cf55b719a6f
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-09-25 23:24:28 +0530

    certification backend pending and issued

--- Node-Backend/routes/certification.js
+++ Node-Backend/routes/certification.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const requestCertification = require('../models/certification');
 const config = require('../config/database');
 
-
+/*save date to requesting certificate data on databade */
 router.post("/requestCert", function (req, res) {
-    // console.log("Hello");
     const newRequest = new requestCertification({
         userid: req.body.userid,
         certName: req.body.certName,
@@ -26,5 +25,39 @@ router.post("/requestCert", function (req, res) {
     });
 });
 
+//get pending certificate to issued
+router.get("/pendingCert", function(req, res){
+    console.log("Hello");
+    requestCertification.find({ state: "Pending" })
+        .select('userid certName certType examName examYear examIndex state')
+        .exec()
+        .then(docs => {
+            console.log("Data Transfer Success.!");
+            res.status(200).json(docs);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
+})
 
+//get issued all certificates 
+router.get("/issuedCert", function(req, res){
+    console.log("Hello");
+    requestCertification.find({ state: "Issued" })
+        .select('userid certName certType examName examYear examIndex state')
+        .exec()
+        .then(docs => {
+            console.log("Data Transfer Success.!");
+            res.status(200).json(docs);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
+})
 module.exports = router; 
