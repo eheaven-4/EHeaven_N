53eac856d815fe5bb6d154a0013c1c2fe48cb718
Pradeepa Sandaruwan <pradeepasandharuwan@gmail.com>
2019-09-26 18:12:40 +0530

    load data from ts file to view file

--- Angular-Frontend/src/app/component/Base_Components/attendance/attendance.component.ts
+++ Angular-Frontend/src/app/component/Base_Components/attendance/attendance.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attendance.component.scss']
 })
 export class AttendanceComponent implements OnInit {
+  public students=[{"name":"Pradeepa1","flag":false},
+  {"name":"Pradeepa2","flag":true},
+  {"name":"Pradeepa3","flag":true},
+  {"name":"Pradeep4","flag":true},
+  {"name":"Pradeep5","flag":true},
+  {"name":"Pradeepa6","flag":true},
+  {"name":"Pradeepa7","flag":false},
+  {"name":"Pradeepa8","flag":true},
+  {"name":"Pradeepa9","flag":true},
+  {"name":"Pradeepa10","flag":true},
+  {"name":"Pradeepa11","flag":true},
+  {"name":"Pradeepa12","flag":true},
+  {"name":"Pradeepa13","flag":true},
+  {"name":"Pradeepa14","flag":true},
+  {"name":"Pradeepa15","flag":true},
+  {"name":"Pradeepa16","flag":true},
+  {"name":"Pradeepa17","flag":true},
+  {"name":"Pradeepa18","flag":true}
+];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  storeValue(name:string,val:boolean) {
+      var value="";
+      if(val){
+        value="Present";
+      }else{
+        value="Absence";
+      }
+      console.log(name+" is "+value);   
   }
 
 }
