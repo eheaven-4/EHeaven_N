ae926cac112cd6542e6a06c1d063363936888d52
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-09-28 20:23:42 +0530

    add font awesom icon

--- Angular-Frontend/src/app/app.module.ts
+++ Angular-Frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatModule } from './material.theme';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CookieService } from 'ngx-cookie-service';
@@ -62,6 +62,7 @@ import { AttendanceComponent } from './component/Base_Components/attendance/atte
     MatDatepickerModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FontAwesomeModule
   ],
   providers: [
     CookieService,
