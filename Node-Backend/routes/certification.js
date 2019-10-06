a616aa4c0ac668466bf7487d1057c20802e6138d
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-10-06 14:44:02 +0530

    a

--- Node-Backend/routes/certification.js
+++ Node-Backend/routes/certification.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {requestCertification} = require('../models/certification');
-const {requestStudentstatus} = require('../models/certification');
+const { requestCertification } = require('../models/certification');
+const { requestStudentstatus } = require('../models/certification');
 const config = require('../config/database');
 const pdfDoc = require('pdf-lib');
 const fs = require('fs');
@@ -15,7 +15,7 @@ router.post("/requestCert", function (req, res) {
         examName: req.body.examName,
         examYear: req.body.examYear,
         examIndex: req.body.examIndex,
-        reqDate:req.body.reqDate,
+        reqDate: req.body.reqDate,
         state: req.body.state
     });
     // console.log(newRequest);
@@ -35,7 +35,7 @@ router.post("/requestCert", function (req, res) {
 router.get("/pendingCert/:id", function (req, res) {
     console.log("Hello");
     const id = req.params.id;
-    requestCertification.find({ state: "Pending" , userid: id})
+    requestCertification.find({ state: "Pending", userid: id })
         .sort({ _id: 1 })
         .select('userid certName certType examName examYear examIndex reqDate state')
         .exec()
@@ -72,52 +72,52 @@ router.get("/issuedCert/:id", function (req, res) {
 });
 
 //test function to generate student status pdf
-router.post("/studentstatus", async function (req, res)  {
+router.post("/studentstatus", async function (req, res) {
     console.log("hello at server ");
     const newRequest = new requestStudentstatus({
         studentName: req.body.studentName,
         admissionNum: req.body.admissionNum,
         dateofAdmission: req.body.dateofAdmission,
         description: req.body.description,
-        state : req.body.state
+        state: req.body.state
     });
     console.log(newRequest);
-    const uint8Array = fs.readFileSync(__dirname  +'/student.pdf')
+    const uint8Array = fs.readFileSync(__dirname + '/student.pdf')
     var doc = await pdfDoc.PDFDocument.load(uint8Array);
     const pages = doc.getPages()
     const page = pages[0];
     page.drawText(
-        newRequest.studentName,    
+        newRequest.studentName,
         {
             x: 200,
-          y: 615,
-          size: 12,
+            y: 615,
+            size: 12,
         },
-        );
+    );
     page.drawText(
-        newRequest.admissionNum,    
+        newRequest.admissionNum,
         {
             x: 215,
             y: 447,
             size: 12,
         },
-        );
+    );
     page.drawText(
-        newRequest.dateofAdmission,    
+        newRequest.dateofAdmission,
         {
             x: 240,
             y: 580,
             size: 12,
         },
-        );
+    );
     page.drawText(
-        newRequest.description,    
+        newRequest.description,
         {
             x: 60,
             y: 510,
             size: 12,
         },
-        );
+    );
     const pdfBytes = await doc.save()
     fs.writeFileSync(__dirname + "studentEdit.pdf", pdfBytes)
 
