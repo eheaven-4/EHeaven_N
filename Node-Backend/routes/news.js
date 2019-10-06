ff57bac1e67dcfda42b094deebce3e34e6b29ce0
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2019-10-06 08:15:31 +0530

    add new form for news component
    news view for admin

--- Node-Backend/routes/news.js
+++ Node-Backend/routes/news.js
@@ -4,5 +4,10 @@ const news = require('../models/news');
 const config = require('../config/database');
 
 
+router.post("/add", function(req, res){
+    console.log("hello");
+    
 
+
+});
 module.exports = router;
