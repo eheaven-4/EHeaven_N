e83e67fa0e0c8f432330474b2b3f9d8f7db73267
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2019-10-06 10:55:47 +0530

    backend on manage clss room

--- Angular-Frontend/src/app/component/Base_Components/manage-marks/manage-marks.component.ts
+++ Angular-Frontend/src/app/component/Base_Components/manage-marks/manage-marks.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+interface classTeacher{
+  _id : String,
+  ctName : String
+}
 
 @Component({
   selector: 'app-manage-marks',
@@ -8,26 +14,49 @@ import { Validators, FormBuilder } from '@angular/forms';
 })
 export class ManageMarksComponent implements OnInit {
 
+  className : String;
+  ctName : classTeacher [] = [];
+
   constructor(
     private fb: FormBuilder,
+    private http: HttpClient,
   ) { }
-// certificate types
-certificates = [
-  'Student Status Verification Certificate',
-  'Character Certificate',
-  'Leaving Certificate',
-  'Educational Certificate'
-];
-CertificationForm = this.fb.group({
-  certName: ['', Validators.required],
-  certType: ['', Validators.required],
-  exam: this.fb.group({
-    examName: ['', Validators.required],
-    examYear: ['', Validators.required],
-    examIndex: ['', Validators.required]
-  })
-});
+  // certificate types
+  certificates = [
+    'Student Status Verification Certificate',
+    'Character Certificate',
+    'Leaving Certificate',
+    'Educational Certificate'
+  ];
+  CertificationForm = this.fb.group({
+    certName: ['', Validators.required],
+    certType: ['', Validators.required],
+    exam: this.fb.group({
+      examName: ['', Validators.required],
+      examYear: ['', Validators.required],
+      examIndex: ['', Validators.required]
+    })
+  });
+
   ngOnInit() {
+    var url = "http://localhost:3000/class_management/classRoomsNames";
+
+    this.http.get<any>(url).subscribe(data => {
+      console.log(data);
+    });
   }
 
+  searchStudents(event, className){
+    
+    const cName = className;
+    console.log(cName)
+
+    var url1 ="http://localhost:3000/class_management/getClassTeacherName"
+    // var url2 = ""
+
+    this.http.get<any>(url1 + '/' + cName).subscribe(res => {
+      this.ctName = res
+      console.log(res)
+    });
+  }
 }
--- Node-Backend/routes/class_management.js
+++ Node-Backend/routes/class_management.js
@@ -19,4 +19,40 @@ router.post("/timeTableRegistration", function (req, res) {
         })
 });
 
+//get all the names and class teachers names of class rooms 
+router.get("/classRoomsNames", function (req, res) {
+    console.log("Hello ")
+    classTimeTable.find()
+        .select('className classTeacher')
+        .exec()
+        .then(docs => {
+            console.log("Data Transfer Success.!")
+            res.status(200).json(docs)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                error: error
+            })
+        })
+});
+
+//get Teachers name using class name 
+router.get("/getClassTeacherName/:cName", function(req,res) {
+    const cName = req.params.cName;
+    classTimeTable.findOne({className : cName})
+        .select('classTeacher')
+        .exec()
+        .then (docs=> {
+            console.log("Data Transer Success..!")
+            res.status(200).json(docs)
+        })
+        .catch(error=> {
+            console.log(error)
+            res.status(500).json({
+                error : error
+            })
+        })
+});
+
 module.exports = router; 
