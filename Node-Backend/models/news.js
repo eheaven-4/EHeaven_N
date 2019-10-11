6f39cebadf51860af14a49aaaf28a54619bf5200
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2019-10-11 21:07:10 +0530

    Update news.js
    news backend

--- Node-Backend/models/news.js
+++ Node-Backend/models/news.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+
+
+const newsSchema = mongoose.Schema({
+    topic:{type:String, require:true},
+    newssumery:{type:String, require:true},
+    news:{type:String, require:true},
+    date:{type:String, require:true},
+});
