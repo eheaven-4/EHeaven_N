5bca195c11c017ec26547667323d47f8b533c138
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2019-10-14 13:39:10 +0530

    news frontend update

--- Node-Backend/routes/news.js
+++ Node-Backend/routes/news.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const news = require('../models/news');
-const config = require('../config/database');
+//const config = require('../config/database');
 
-
+/*save date to  data on databade */
 router.post("/add",(req, res)=>{
     console.log("hello");
 
     const newNews = new News({
-        
+        userid:req.body.userid,
+        topic:req.body.topic,
+        newssumery:req.body.newssumery,
+        news:req.body.news,
+        date:req.body.date,
+        filepath:req.body.filepath
 
+    });
+
+    newNews
+    .save()
+    .then(result => {
+        console.log(result)
+        res.json({ state: true, msg: "Data inserted Successfully..!" });
+    })
+    .catch(error => {
+        console.log(error)
+        res.json({ state: false, msg: "Data inserting Unsuccessfull..!" });
     })
 
 
