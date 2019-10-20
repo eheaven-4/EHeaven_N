5b92ce0f0e45e675fcc846cd5013cf177cdc51c9
sachintsm <sachintsmuthumala@gmail.com>
2019-10-20 23:09:06 +0530

    Academic component UI done

--- Angular-Frontend/src/app/component/Base_Components/academics/academics.component.ts
+++ Angular-Frontend/src/app/component/Base_Components/academics/academics.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgFlashMessageService } from 'ng-flash-messages';
 import { MycookiesService } from '../../Admin/mycookies.service';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-academics',
@@ -12,14 +13,16 @@ import { MycookiesService } from '../../Admin/mycookies.service';
 
 export class AcademicsComponent implements OnInit {
 
-
+  faFile = faFile;
+  
   constructor(
     private http: HttpClient,
     private ngFlashMessageService: NgFlashMessageService,
     private cookies: MycookiesService,
   ) { }
 
-
+  grades = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+  attachmentType = ['Tutorial', 'Lectuer Slide', 'Resouses', 'Other'];
 
   ngOnInit() {
 
