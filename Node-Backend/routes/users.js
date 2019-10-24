a93ea22f598484036ce8a1731f31968bea32d9ee
sachintsm <sachintsmuthumala@gmail.com>
2019-10-24 23:10:48 +0530

    fix error

--- Node-Backend/routes/users.js
+++ Node-Backend/routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {User} = require('../models/users');
-const {bulkUser} = require('../models/users');
+const User = require('../models/users');
+
+// const {User} = require('../models/users');
+// const {bulkUser} = require('../models/users');
+const {findByUserid} = require('../models/users')
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const multer = require('multer');
@@ -27,13 +30,13 @@ var bulkStorage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage }).single('profileImage');
-const uploadBulk = multer({ storage : bulkStorage}).single('bulk_req_file');
+// const uploadBulk = multer({ storage : bulkStorage}).single('bulk_req_file');
 
 //user login scope
 router.post("/login", function (req, res, next) {
     const userid = req.body.userid;
     const password = req.body.password;
-
+    // const query = { userid: userid };
     User.findByUserid(userid, function (err, user) {
         if (err) throw err;
         if (!user) {
@@ -168,28 +171,28 @@ router.get("/getStudentsNames/:cName", function (req, res, next) {
         })
 })
 
-router.post("/bulkRegistration", function( req, res, next){
-    uploadBulk(req, res, (err) => { //uploading file to the notification_Attachment folder and 
+// router.post("/bulkRegistration", function( req, res, next){
+//     uploadBulk(req, res, (err) => { //uploading file to the notification_Attachment folder and 
 
-        if (req.file) {
-            var filePath = "NOT_FILE - " + req.file.originalname;    //send data to the database
-            //get file name 
-        }
+//         if (req.file) {
+//             var filePath = "NOT_FILE - " + req.file.originalname;    //send data to the database
+//             //get file name 
+//         }
 
-        const newNotice = new bulkUser({
-            name: req.body.name,
-            filepath: filePath
-        });
+//         const newNotice = new bulkUser({
+//             name: req.body.name,
+//             filepath: filePath
+//         });
 
-        newNotice.save()
-            .then(result => {
-                console.log(result)
-                res.json({ state: true, msg: "Data Inserted Successfully..!" });
-            })
-            .catch(error => {
-                console.log(error)
-                res.json({ state: false, msg: "Data Inserting Unsuccessfull..!" });
-            })
-    });
-})
+//         newNotice.save()
+//             .then(result => {
+//                 console.log(result)
+//                 res.json({ state: true, msg: "Data Inserted Successfully..!" });
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//                 res.json({ state: false, msg: "Data Inserting Unsuccessfull..!" });
+//             })
+//     });
+// })
 module.exports = router;  
