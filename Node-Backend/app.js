4e334411a1970e5a18938ea96b8ea3eb7dadcd9f
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2019-10-31 17:48:00 +0530

    New

--- Node-Backend/app.js
+++ Node-Backend/app.js
@@ -18,7 +18,7 @@ const news = require('./routes/news');
 const filehandler= require('./routes/filehandler');
 
 app.use(cors());
-
+// saskaojsaoijao
 const connection = mongoose.connect(config.database);
 if(connection){
     console.log("Database Connected");
