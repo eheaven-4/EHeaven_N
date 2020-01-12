03f522b65d252073b79931d95fdf0cdd9a61859c
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-01-12 18:38:49 +0530

    contact us backend completed

--- Node-Backend/routes/contact_us.js
+++ Node-Backend/routes/contact_us.js
@@ -4,33 +4,84 @@ const ContactUs = require('../models/contact_us');
 const config = require('../config/database');
 
 
+//add new message
 router.post("/sendMessage", function(req,res) {
 
     console.log(req.body);
     
-    // const newData = new ContactUs({
-    //     name :  req.body.name,
-    //     email : req.body.email,
-    //     mobile : req.body.mobile,
-    //     nic : req.body.nic,
-    //     subject : req.body.subject,
-    //     message : req.body.message,
-    //     date : Date(),
-    //     state : "pending"
-    // })
-    // console.log(newData);
+    const newData = new ContactUs({
+        name :  req.body.name,
+        email : req.body.email,
+        mobile : req.body.mobile,
+        nic : req.body.nic,
+        subject : req.body.subject,
+        message : req.body.message,
+        date : Date(),
+        state : "pending"
+    })
+    console.log(newData);
     
-    // const newMesage = new ContactUs(newData)
-    // console.log(newMesage)
-    // newMesage.save()
-    //     .then(result => {
-    //         console.log(result)
-    //         res.json({ state: true, msg: "Data Inserted Successfully..!" });
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //         res.json({ state: false, msg: "Data Inserting Unsuccessfull..!" });
-    //     })
+    const newMesage = new ContactUs(newData)
+    console.log(newMesage)
+    newMesage.save()
+        .then(result => {
+            console.log(result)
+            res.json({ state: true, msg: "Data Inserted Successfully..!" });
+        })
+        .catch(error => {
+            console.log(error)
+            res.json({ state: false, msg: "Data Inserting Unsuccessfull..!" });
+        })
 })
 
+//get all messages
+router.get("/allMessages" , function(req,res){
+    ContactUs.find().sort({ date: -1 })
+    .select('name subject message date state mobile nic email')
+    .exec()
+    .then(docs => {
+        console.log("Data Transfer Success.!");
+        res.status(200).json(docs);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: error
+        });
+    });
+})
+
+//get one message
+router.get("/viewMessage/:id", function(req,res) {
+    const id = req.params.id;
+    ContactUs.findOne({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
+})
+
+//delete message
+router.delete("/deleteMessage/:id", function(req,res) {
+    const id = rq.params.id;
+    ContactUs.remove({_id : id})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message : "Successfully deleted !"
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error : error
+            })
+        })
+})
 module.exports = router;
