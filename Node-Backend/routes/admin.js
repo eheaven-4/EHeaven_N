ed554349c8b016bc80779a121d70c6f565e98cd7
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-01-14 12:03:59 +0530

    admin js

---
+++ Node-Backend/routes/admin.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const academicStuff = require('../models/admin');
+const config = require('../config/database');
+
+
+
+module.exports = router;
